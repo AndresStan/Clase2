d776ea855ebd031ea96cba1a48cabdd06780fba9
Andres clase <aastan@iesch.org>
2026-09-17 18:23:28 +0200

    AutoPush

--- moviles/EdadCanina/app/src/main/java/org/iesch/edadcanina/MainActivity.kt
+++ moviles/EdadCanina/app/src/main/java/org/iesch/edadcanina/MainActivity.kt
@@ -1,6 +1,10 @@
 package org.iesch.edadcanina
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,5 +24,25 @@ class MainActivity : AppCompatActivity() {
         // Aqui la pantalla esta creada
 
         // 1 - Tomamos el control de todos los elementos de la parte de la UI
+        val textoRespuesta = findViewById<TextView>(R.id.textoRespuesta)
+        val botonCalcular = findViewById<Button>(R.id.botonCalcular)
+        val ageEdit = findViewById<EditText>(R.id.edit_edad)
+
+        // 2 - Los botones tienen la propiedad setOnClickListener al pulsarlo
+        botonCalcular.setOnClickListener {
+
+
+
+            if (!ageEdit.text.toString().isEmpty()){
+                val edadString = ageEdit.text.toString()
+                val edadInt = edadString.toInt()*7
+                textoRespuesta.text = getString(R.string.textoRespuesta, edadInt)
+            } else {
+                Toast.makeText(this, R.string.texto_toast, Toast.LENGTH_SHORT).show()
+            }
+
+
+
+        }
     }
 }
